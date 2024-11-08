a36a6c073ec6f758b78db699b37593d6965d5f8b
Arthur Felipe Rodrigues Costa <arturfelipe93@gmail.com>
2024-11-08 13:47:52 -0300

    Update equalize.cpp

--- exercicio_12/equalize.cpp
+++ exercicio_12/equalize.cpp
@@ -34,6 +34,29 @@ int main(int argc, char** argv) {
   cv::Mat histImg(histh, histw, CV_8UC3, cv::Scalar(0,0,0));
 
   while(true) {
-    
+    cap >> image;
+
+    cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY);
+
+    cv::calcHist(&gray_image, 1, 0, cv::Mat(), hist, 1, &nbins, &histrange, uniform, acummulate);
+    cv::normalize(hist, hist, 0, histImg.rows, cv::NORM_MINMAX, -1, cv::Mat());
+
+    histImg.setTo(cv::Scalar(0));
+
+    for (int i = 0; i < nbins; i++) {
+      cv::line(histImg,
+               cv::Point(i, histh),
+               cv::Point(i, histh - cvRound(hist.at<float>(i))),
+               cv::Scalar(255,255,255),1,8,0);
+    }
+
+    cv::equalizeHist(gray_image, equalized_image);
+
+    cv::imshow("Imagem equalizada", equalized_image);
+    cv::imshow("Histograma", histImg);
+
+    key = CV::waitKey(30);
+    if (key == 27) true;
   }
+  return 0;
 }
